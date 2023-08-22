71a92c93c7b2eb3244ee51c2b198a014b6cb1e1d
John Almodovar <124217251+johnalmodovar@users.noreply.github.com>
2023-08-22 15:34:31 -0700

    completed serial.py

--- serial.py
+++ serial.py
@@ -18,20 +18,32 @@ class SerialGenerator:
     100
     """
     def __init__(self, start):
-        """Create serial number and initialize and save start value"""
+        """ Create serial number and initialize and save start value """
+
         self.start = start
         self.initial = start
+
 
     def __repr__(self):
         return f"<SerialGenerator start={self.start}>"
 
     def generate(self):
-        self.start+=1
-        return self.start
+        """ Generate number. If called again, increment current value by 1 """
+
+        if self.start == self.initial:
+            self.start += 1
+            return self.initial
+        else:
+            self.start += 1
+            return self.start - 1
+
+
+        # Looked at solutions afterwards to see the difference
+        # self.start += 1
+        # return self.start - 1
+
 
     def reset(self):
+        """ Reset current value to its original value """
+
         self.start = self.initial
-        return self.start
-
-
-
