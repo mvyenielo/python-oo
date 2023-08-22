67f55d87d7dfd1139305923f7a75d5dd99c10672
John Almodovar <124217251+johnalmodovar@users.noreply.github.com>
2023-08-22 16:42:13 -0700

    completed wordfinder

--- wordfinder.py
+++ wordfinder.py
@@ -1,12 +1,27 @@
+import random
+
 class WordFinder:
-    """Word Finder: finds random words from a dictionary."""
-def __init__(self, path):
-    self.path = path
-    self.word_list = read_file()
-    #file.close()
+    """ Word Finder: finds random words from a dictionary. """
 
-def read_file(self):
-    file = open(self.path)
-    return file.read()
+    def __init__(self, path):
+        """ Generates list of words """
 
-def random():
+        self.path = path
+        self.word_list = self.read_file()
+
+
+    def read_file(self):
+        """ Creates a file out of the path and converts the file into a list """
+
+
+        file = open(self.path)
+
+        text = file.read()
+
+        return text.split("\n")
+
+
+    def random(self):
+        """ Generates a random word from the word list """
+
+        return random.choice(self.word_list)
