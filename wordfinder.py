4277d9b0ef61a865f7b5403ac5d39d99e439c737
Megan Vyenielo <mvyenielo58@gmail.com>
2023-08-22 16:07:49 -0700

    started wordfinder

--- wordfinder.py
+++ wordfinder.py
@@ -1,2 +1,12 @@
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
+def __init__(self, path):
+    self.path = path
+    self.word_list = read_file()
+    #file.close()
+
+def read_file(self):
+    file = open(self.path)
+    return file.read()
+
+def random():
