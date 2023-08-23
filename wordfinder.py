645acba711ae9a68df0f3b13144d62349d1417df
Megan Vyenielo <mvyenielo58@gmail.com>
2023-08-22 17:44:10 -0700

    completed random word class and subclass

--- wordfinder.py
+++ wordfinder.py
@@ -1,7 +1,13 @@
 import random
 
 class WordFinder:
-    """ Word Finder: finds random words from a dictionary. """
+    """ Word Finder: finds random words from a dictionary.
+
+        TODO: should we do make tests???
+        >>> word = WordFinder(/usr/share/dict/words)
+    """
+
+
 
     def __init__(self, path):
         """ Generates list of words """
@@ -25,3 +31,10 @@ class WordFinder:
         """ Generates a random word from the word list """
 
         return random.choice(self.word_list)
+
+class SpecialWordFinder(WordFinder):
+
+    def read_file(self):
+        whole_list = super().read_file()
+        print(whole_list)
+        return [word for word in whole_list if word != '' and not word.startswith("#")]
